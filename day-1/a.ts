eb02d795d346a28df9aaa9e922f85be3a33df3ce
Andres Meidla <andres.meidla@gmail.com>
2024-12-01 15:38:30 +0200

    feat: day1 solution

--- day-1/a.ts
+++ day-1/a.ts
@@ -1,9 +1,49 @@
 import { runSolution } from '../utils.ts';
 
+const output = (a: number[], b: number[]) => {
+  let str = "";
+  for (let i = 0; i < a.length; i++) {
+    str += `${a[i]} ${b[i]}\n`;
+  }
+
+  console.log(str);
+}
+
+const parseInput = (data: string[]) => {
+  // console.log(data);
+  const [a, b] = data.reduce(([a, b], x) => {
+    const spaceAt = x.indexOf(' ');
+    a.push(parseInt(x.slice(0, spaceAt)));
+    b.push(parseInt(x.slice(spaceAt + 1)));
+    return [a, b];
+  }, [[] as number[], [] as number[]]);
+
+  output(a, b);
+
+  return [a, b];
+}
+
+const calcDistance = (a: number[], b: number[]) => {
+  let sum = 0;
+  for (let i = 0; i < a.length; i++) {
+    const dist = Math.abs(a[i] - b[i]);
+    sum += dist;
+  }
+  return sum;
+}
+
 /** provide your solution as the return of this function */
 export async function day1a(data: string[]) {
-  console.log(data);
-  return 0;
+  const [a, b] = parseInput(data);
+
+  // sort the arrays
+  a.sort((x, y) => x - y);
+  b.sort((x, y) => x - y);
+
+  output(a, b);
+  const distance = calcDistance(a, b);
+
+  return distance;
 }
 
 await runSolution(day1a);
--- day-1/b.ts
+++ day-1/b.ts
@@ -1,9 +1,67 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import { runSolution } from '../utils.ts';
 
-/** provide your solution as the return of this function */
-export async function day1b(data: string[]) {
-  console.log(data);
-  return 0;
+const output = (a: number[], b: number[]) => {
+  let str = "";
+  for (let i = 0; i < a.length; i++) {
+    str += `${a[i]} ${b[i]}\n`;
+  }
+
+  console.log(str);
 }
 
-await runSolution(day1b);
+const parseInput = (data: string[]) => {
+  // console.log(data);
+  const [a, b] = data.reduce(([a, b], x) => {
+    const spaceAt = x.indexOf(' ');
+    a.push(parseInt(x.slice(0, spaceAt)));
+    b.push(parseInt(x.slice(spaceAt + 1)));
+    return [a, b];
+  }, [[] as number[], [] as number[]]);
+
+  output(a, b);
+
+  return [a, b];
+}
+
+const calcDistance = (a: number[], b: number[]) => {
+  let sum = 0;
+  const occ = new Map<number, number>();
+  for (let i = 0; i < a.length; i++) {
+    const numberOnTheRight = b[i];
+    if (!occ.has(numberOnTheRight)) {
+      occ.set(numberOnTheRight, 0);
+    }
+    occ.set(numberOnTheRight, occ.get(numberOnTheRight)! + 1);
+  }
+
+
+  for (let i = 0; i < a.length; i++) {
+    const number = a[i];
+    if (!occ.has(number)) {
+      // console.log(`Number ${number} is not in the right array`);
+    } else {
+      // console.log(`Number ${number} is in the right array ${occ.get(number)} times`);
+      sum += number * occ.get(number);
+    }
+  }
+
+  console.log(occ);
+  return sum;
+}
+
+/** provide your solution as the return of this function */
+export async function day1a(data: string[]) {
+  const [a, b] = parseInput(data);
+
+  // sort the arrays
+  a.sort((x, y) => x - y);
+  b.sort((x, y) => x - y);
+
+  output(a, b);
+  const distance = calcDistance(a, b);
+
+  return distance;
+}
+
+await runSolution(day1a);
