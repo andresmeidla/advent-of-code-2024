85156c3dd6835da99c3afd26ab1d87e119ec1f38
Andres Meidla <andres.meidla@gmail.com>
2024-12-02 10:07:31 +0200

    feat: day 2 solutions

--- day-2/a.ts
+++ day-2/a.ts
@@ -1,9 +1,62 @@
 import { runSolution } from '../utils.ts';
 
+const parseInput = (input: string[]) => {
+  return input.map((line) => {
+    return line.split(' ').map((a) => {
+      return parseInt(a);
+    });
+  });
+};
+
+const checkSafety = (report: number[]) => {
+  let lastOne: number | undefined;
+  let increasing: boolean | undefined;
+  for (let i = 0; i < report.length; i++) {
+    if (lastOne !== undefined) {
+      if (increasing === undefined) {
+        increasing = report[i] > lastOne;
+      }
+
+      // console.log(
+      //   'i',
+      //   i,
+      //   'lastOne',
+      //   lastOne,
+      //   'increasing',
+      //   increasing,
+      //   'report[i]',
+      //   report[i]
+      // );
+      if (increasing && report[i] < lastOne) {
+        return false;
+      }
+      if (!increasing && report[i] > lastOne) {
+        return false;
+      }
+      const diff = Math.abs(report[i] - lastOne);
+      if (diff < 1 || diff > 3) {
+        return false;
+      }
+    }
+
+    lastOne = report[i];
+  }
+  return true;
+};
+
 /** provide your solution as the return of this function */
 export async function day2a(data: string[]) {
-  console.log(data);
-  return 0;
+  const arr = parseInput(data);
+  console.log(arr);
+
+  let count = 0;
+  for (const report of arr) {
+    if (checkSafety(report)) {
+      count += 1;
+    }
+  }
+
+  return count;
 }
 
 await runSolution(day2a);
--- day-2/b.ts
+++ day-2/b.ts
@@ -1,9 +1,68 @@
 import { runSolution } from '../utils.ts';
 
+const parseInput = (input: string[]) => {
+  return input.map((line) => {
+    return line.split(' ').map((a) => {
+      return parseInt(a);
+    });
+  });
+};
+
+const checkSafety = (report: number[]) => {
+  let lastOne: number | undefined;
+  let increasing: boolean | undefined;
+  for (let i = 0; i < report.length; i++) {
+    if (lastOne !== undefined) {
+      if (increasing === undefined) {
+        increasing = report[i] > lastOne;
+      }
+
+      // console.log(
+      //   'i',
+      //   i,
+      //   'lastOne',
+      //   lastOne,
+      //   'increasing',
+      //   increasing,
+      //   'report[i]',
+      //   report[i]
+      // );
+      if (increasing && report[i] < lastOne) {
+        return false;
+      }
+      if (!increasing && report[i] > lastOne) {
+        return false;
+      }
+      const diff = Math.abs(report[i] - lastOne);
+      if (diff < 1 || diff > 3) {
+        return false;
+      }
+    }
+
+    lastOne = report[i];
+  }
+  return true;
+};
+
 /** provide your solution as the return of this function */
-export async function day2b(data: string[]) {
-  console.log(data);
-  return 0;
+export async function day2a(data: string[]) {
+  const arr = parseInput(data);
+  console.log(arr);
+
+  let count = 0;
+  for (const report of arr) {
+    for (let i = 0; i < report.length; ++i) {
+      // remove element on index i
+      const reportCopy = report.slice();
+      reportCopy.splice(i, 1);
+      if (checkSafety(reportCopy)) {
+        count += 1;
+        break; // no need to check further
+      }
+    }
+  }
+
+  return count;
 }
 
-await runSolution(day2b);
+await runSolution(day2a);
