1c0671c88b2c70791696118e0a52368fea487195
Andres Meidla <andres.meidla@gmail.com>
2024-12-04 19:32:00 +0200

    feat: day 4 solution

--- day-4/a.ts
+++ day-4/a.ts
@@ -1,9 +1,121 @@
 import { runSolution } from '../utils.ts';
 
+function findLeftToRight(str: string[]) {
+  let sum = 0;
+  for (const line of str) {
+    const regex = /XMAS/g;
+    const matches = line.match(regex);
+    if (matches) {
+      console.log('matches', matches);
+      sum += matches.length;
+    }
+  }
+  return sum;
+}
+
+function createTopToBottom(str: string[]) {
+  const strings = [];
+  for (let x = 0; x < str[0].length; x++) {
+    let newStr = '';
+    for (let y = 0; y < str.length; y++) {
+      newStr += str[y][x];
+    }
+    strings.push(newStr);
+  }
+  return strings;
+}
+
+function createDiagonalStrings(str: string[]) {
+  const diags: string[] = [];
+  const yLen = str.length;
+  const xLen = str[0].length;
+
+  // left to right and down
+  // first we look at the left side of the grid
+  for (let y = 0; y < yLen; y++) {
+    let diag = '';
+    if (y + 3 < yLen) {
+      for (let x = 0; x < xLen; x++) {
+        if (x + y < yLen) {
+          diag += str[y + x][x];
+        }
+      }
+      diags.push(diag);
+    }
+  }
+  // then we look at the top of the grid
+  for (let x = 1; x < xLen; x++) {
+    let diag = '';
+    if (x + 3 < xLen) {
+      for (let y = 0; y < yLen; y++) {
+        if (x + y < xLen) {
+          diag += str[y][x + y];
+        }
+      }
+      diags.push(diag);
+    }
+  }
+
+  return diags;
+}
+
 /** provide your solution as the return of this function */
-export async function day4a(data: string[]) {
-  console.log(data);
-  return 0;
+export async function day4a(dataIn: string[]) {
+  const data = dataIn.filter((d) => d.length > 0);
+  console.log(data.join('\n') + '\n');
+  let sum = 0;
+  {
+    const s = findLeftToRight(data);
+    console.log('left to right', s);
+    sum += s;
+  }
+  {
+    const reversed = data.map((d) => d.split('').reverse().join(''));
+    const s = findLeftToRight(reversed);
+    console.log('right to left', s);
+    sum += s;
+  }
+  {
+    const topToBottom = createTopToBottom(data);
+    const s = findLeftToRight(topToBottom);
+    console.log('top to bottom', s);
+    sum += s;
+  }
+  {
+    const reversed = createTopToBottom(data).map((d) =>
+      d.split('').reverse().join('')
+    );
+    const s = findLeftToRight(reversed);
+    console.log('bottom to top', s);
+    sum += s;
+  }
+  const diags = createDiagonalStrings(data);
+  const otherDiags = createDiagonalStrings(
+    data.map((d) => d.split('').reverse().join(''))
+  );
+  {
+    const s = findLeftToRight(diags);
+    console.log('diags left to right and down', s);
+    sum += s;
+  }
+  {
+    const s = findLeftToRight(otherDiags);
+    console.log('diags right to left and down', s);
+    sum += s;
+  }
+  {
+    const s = findLeftToRight(diags.map((d) => d.split('').reverse().join('')));
+    console.log('diags left to right and up', s);
+    sum += s;
+  }
+  {
+    const s = findLeftToRight(
+      otherDiags.map((d) => d.split('').reverse().join(''))
+    );
+    console.log('diags right to left and up', s);
+    sum += s;
+  }
+  return sum;
 }
 
 await runSolution(day4a);
--- day-4/b.ts
+++ day-4/b.ts
@@ -1,9 +1,78 @@
 import { runSolution } from '../utils.ts';
 
+function findMAS(str: string[], x: number, y: number) {
+  // the MAX needs to be in a X formation like this:
+  // M_S
+  // _A_
+  // M_S
+  // there needs to be 3 characters to the right and 3 characters down for a possible formation
+
+  if (
+    str[y][x] === 'M' &&
+    str[y][x + 2] === 'S' &&
+    str[y + 1][x + 1] === 'A' &&
+    str[y + 2][x] === 'M' &&
+    str[y + 2][x + 2] === 'S'
+  ) {
+    return 1;
+  }
+
+  if (
+    str[y][x] === 'M' &&
+    str[y][x + 2] === 'M' &&
+    str[y + 1][x + 1] === 'A' &&
+    str[y + 2][x] === 'S' &&
+    str[y + 2][x + 2] === 'S'
+  ) {
+    return 2;
+  }
+
+  if (
+    str[y][x] === 'S' &&
+    str[y][x + 2] === 'S' &&
+    str[y + 1][x + 1] === 'A' &&
+    str[y + 2][x] === 'M' &&
+    str[y + 2][x + 2] === 'M'
+  ) {
+    return 3;
+  }
+
+  if (
+    str[y][x] === 'S' &&
+    str[y][x + 2] === 'M' &&
+    str[y + 1][x + 1] === 'A' &&
+    str[y + 2][x] === 'S' &&
+    str[y + 2][x + 2] === 'M'
+  ) {
+    return 4;
+  }
+
+  return 0;
+}
+
+function findAllMAS(str: string[]) {
+  let sum = 0;
+  for (let y = 0; y < str.length; y++) {
+    for (let x = 0; x < str[0].length; x++) {
+      // there needs to be 3 characters to the right and 3 characters down for a possible formation
+      if (x + 2 < str[0].length && y + 2 < str.length) {
+        let a;
+        if ((a = findMAS(str, x, y)) > 0) {
+          console.log('FOUND MAS y', y, 'x', x, 'a', a);
+          sum++;
+        }
+      }
+    }
+  }
+  return sum;
+}
+
 /** provide your solution as the return of this function */
 export async function day4b(data: string[]) {
-  console.log(data);
-  return 0;
+  console.log(data.join('\n') + '\n');
+  const sum = findAllMAS(data);
+
+  return sum;
 }
 
 await runSolution(day4b);
