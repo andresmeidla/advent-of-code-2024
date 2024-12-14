19ccc5d4878f59633eb059a6d3b4b7a932fa2274
Andres Meidla <andres.meidla@gmail.com>
2024-12-14 08:49:07 +0200

    feat: solution for day 14

--- day-14/a.ts
+++ day-14/a.ts
@@ -1,9 +1,111 @@
 import { runSolution } from '../utils.ts';
 
+type Robot = {
+  pos: {
+    x: number;
+    y: number;
+  };
+  velocity: {
+    x: number;
+    y: number;
+  };
+};
+
+type MapNode = {
+  x: number;
+  y: number;
+  robots: Robot[];
+};
+
 /** provide your solution as the return of this function */
 export async function day14a(data: string[]) {
   console.log(data);
-  return 0;
+  const robots: Robot[] = [];
+  for (const line of data) {
+    const [leftToken, rightToken] = line.split(' ').map((x) => x.slice(2));
+    robots.push({
+      pos: {
+        x: parseInt(leftToken.split(',')[0]),
+        y: parseInt(leftToken.split(',')[1]),
+      },
+      velocity: {
+        x: parseInt(rightToken.split(',')[0]),
+        y: parseInt(rightToken.split(',')[1]),
+      },
+    });
+  }
+  console.log(robots);
+  const MAP_WIDTH = 101;
+  const MAP_HEIGHT = 103;
+  const map: MapNode[][] = [];
+  for (let y = 0; y < MAP_HEIGHT; y++) {
+    map.push([]);
+    for (let x = 0; x < MAP_WIDTH; x++) {
+      map[y].push({
+        x,
+        y,
+        robots: [],
+      });
+    }
+  }
+
+  // move robots
+  const SECONDS = 100;
+
+  for (const robot of robots) {
+    let x = (robot.pos.x + robot.velocity.x * 100) % MAP_WIDTH;
+    let y = (robot.pos.y + robot.velocity.y * 100) % MAP_HEIGHT;
+    if (x < 0) {
+      x = (MAP_WIDTH + x) % MAP_WIDTH;
+    }
+    if (y < 0) {
+      y = (MAP_HEIGHT + y) % MAP_HEIGHT;
+    }
+    console.log(y, x);
+    map[y][x].robots.push(robot);
+  }
+  // print map
+  for (let y = 0; y < MAP_HEIGHT; y++) {
+    let line = '';
+    for (let x = 0; x < MAP_WIDTH; x++) {
+      if (map[y][x].robots.length > 0) {
+        line += map[y][x].robots.length;
+      } else {
+        line += '.';
+      }
+    }
+    console.log(line);
+  }
+
+  // count quadrant robot counts, ignoring the lines that are in the middles of the quadrants
+  const counts = [0, 0, 0, 0];
+  for (let y = 0; y < MAP_HEIGHT; y++) {
+    for (let x = 0; x < MAP_WIDTH; x++) {
+      if (map[y][x].robots.length > 0) {
+        if (x === Math.floor(MAP_WIDTH / 2)) {
+          continue;
+        }
+        if (y === Math.floor(MAP_HEIGHT / 2)) {
+          continue;
+        }
+        if (x < Math.floor(MAP_WIDTH / 2) && y < Math.floor(MAP_HEIGHT / 2)) {
+          counts[0] += map[y][x].robots.length;
+        }
+        if (x >= Math.floor(MAP_WIDTH / 2) && y < Math.floor(MAP_HEIGHT / 2)) {
+          counts[1] += map[y][x].robots.length;
+        }
+        if (x < Math.floor(MAP_WIDTH / 2) && y >= Math.floor(MAP_HEIGHT / 2)) {
+          counts[2] += map[y][x].robots.length;
+        }
+        if (x >= Math.floor(MAP_WIDTH / 2) && y >= Math.floor(MAP_HEIGHT / 2)) {
+          counts[3] += map[y][x].robots.length;
+        }
+      }
+    }
+  }
+  console.log(counts);
+
+  return counts.reduce((acc, x) => acc * x, 1);
 }
 
 await runSolution(day14a);
--- day-14/b.ts
+++ day-14/b.ts
@@ -1,9 +1,122 @@
 import { runSolution } from '../utils.ts';
 
+type Robot = {
+  id: number;
+  pos: {
+    x: number;
+    y: number;
+  };
+  velocity: {
+    x: number;
+    y: number;
+  };
+};
+
+type MapNode = {
+  x: number;
+  y: number;
+  robots: Robot[];
+};
+
 /** provide your solution as the return of this function */
-export async function day14b(data: string[]) {
-  console.log(data);
+export async function day14a(data: string[]) {
+  const robots: Robot[] = [];
+  let id = 0;
+  for (const line of data) {
+    const [leftToken, rightToken] = line.split(' ').map((x) => x.slice(2));
+    robots.push({
+      id: id++,
+      pos: {
+        x: parseInt(leftToken.split(',')[0]),
+        y: parseInt(leftToken.split(',')[1]),
+      },
+      velocity: {
+        x: parseInt(rightToken.split(',')[0]),
+        y: parseInt(rightToken.split(',')[1]),
+      },
+    });
+  }
+  const MAP_WIDTH = 101;
+  const MAP_HEIGHT = 103;
+  const map: MapNode[][] = [];
+  for (let y = 0; y < MAP_HEIGHT; y++) {
+    map.push([]);
+    for (let x = 0; x < MAP_WIDTH; x++) {
+      map[y].push({
+        x,
+        y,
+        robots: [],
+      });
+    }
+  }
+
+  const printMap = () => {
+    // print map
+    for (let y = 0; y < MAP_HEIGHT; y++) {
+      let line = '';
+      for (let x = 0; x < MAP_WIDTH; x++) {
+        if (map[y][x].robots.length > 0) {
+          line += 'X ';
+        } else {
+          line += '. ';
+        }
+      }
+      console.log(line);
+    }
+  };
+
+  // move robots
+
+  for (const robot of robots) {
+    let x = (robot.pos.x + robot.velocity.x * 100) % MAP_WIDTH;
+    let y = (robot.pos.y + robot.velocity.y * 100) % MAP_HEIGHT;
+    if (x < 0) {
+      x = (MAP_WIDTH + x) % MAP_WIDTH;
+    }
+    if (y < 0) {
+      y = (MAP_HEIGHT + y) % MAP_HEIGHT;
+    }
+    map[y][x].robots.push(robot);
+  }
+
+  const SECONDS = 10000;
+  for (let i = 0; i < SECONDS; i++) {
+    map.forEach((row) => row.forEach((node) => (node.robots = [])));
+    for (const robot of robots) {
+      let x = (robot.pos.x + robot.velocity.x * i) % MAP_WIDTH;
+      let y = (robot.pos.y + robot.velocity.y * i) % MAP_HEIGHT;
+      if (x < 0) {
+        x = (MAP_WIDTH + x) % MAP_WIDTH;
+      }
+      if (y < 0) {
+        y = (MAP_HEIGHT + y) % MAP_HEIGHT;
+      }
+      map[y][x].robots.push(robot);
+    }
+
+    // find 10 consecutive X-es in a row
+    let found = false;
+    for (let y = 0; y < MAP_HEIGHT; y++) {
+      let count = 0;
+      for (let x = 0; x < MAP_WIDTH; x++) {
+        if (map[y][x].robots.length > 0) {
+          count++;
+        } else {
+          count = 0;
+        }
+        if (count === 10) {
+          console.log('found at', x, y, 'i=', i);
+          found = true;
+          break;
+        }
+      }
+    }
+    if (found) {
+      printMap();
+    }
+  }
+
   return 0;
 }
 
-await runSolution(day14b);
+await runSolution(day14a);
